efa0e4b9894eef0b275e8c23b796f2d398e12904
M31739 <eapen.kuruvilla@noblis.org>
2026-01-13 15:34:36 -0500

    Test position from pgn.zst. Positions : 100000   MSE:  0.041909   MAE:  0.113207

--- cached_board.py
+++ cached_board.py
@@ -16,8 +16,10 @@ from copy import deepcopy
 
 from defunc_prepare_data import BOARD_SHAPE, PIECE_TO_PLANE, PIECE_VALUES
 
+# TODO Use https://github.com/zedeckj/bulletchess?tab=readme-ov-file
 # TODO Use cython to fix bottleneck functions
-# TODO or use https://github.com/thefcraft/FastChess
+# TODO or use https://github.com/thefcraft/FastChess - No zobrist hash
+
 # ========== PST Tables ==========
 # fmt: off
 PST_PAWN = [
